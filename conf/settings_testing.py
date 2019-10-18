b9ae68f4b3459180c9aafb861e121b954f3f0d86
周长良 <zhoucl@kaopuyun.com>
2019-10-18 14:16:49 +0800

    update

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -19,7 +19,7 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+        'NAME': 'test-my-app',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'bk',                            # 你的数据库user
         'PASSWORD': '123456',                        # 你的数据库password
         'HOST': '192.168.78.108',                   		   # 数据库HOST
